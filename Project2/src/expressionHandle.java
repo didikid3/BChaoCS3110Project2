74fd704603f55364adb773f3f618a9ee1aa6166c
didikid3 <34197234+didikid3@users.noreply.github.com>
2022-04-29 21:34:11 -0700

    Modify state 1 to also handle first value of decimal (".")

--- Project2/src/expressionHandle.java
+++ Project2/src/expressionHandle.java
@@ -1,33 +1,39 @@
 
+import java.util.Scanner;
 import java.util.Stack;
+
 public class expressionHandle {
 
 	
-	public int evalEx(String input) {
+	public double[] evalEx(String input) {
 		
+		double[] result = new double[2];
+		result[0] = 1;
+		result[1] = 0;
 		int size = input.length();
 		char curr;
 		//My factors w/o including '('expression')'
-		Stack<Integer> values = new Stack<Integer>();
+		Stack<Double> values = new Stack<>();
 		//My operators
-		Stack<Character> operator = new Stack<Character>();
-		
+		Stack<Character> operator = new Stack<>();
+		floatPointValue machine = new floatPointValue();
 		for(int i = 0; i < size; i++) {
 			curr = input.charAt(i);
 			
 			//If its a numeric value
-			if(curr >= '0' && curr <= '9') {
-				String x = "";
+			//Now Handle 
+			if((curr >= '0' && curr <= '9') || curr == '.') {
 				
-				//Group all the numbers into one string
-				while (i < size && curr >= '0' && curr <= '9') {
-					x += curr;
-					i+= 1;
+				//Run modified floatPointLiteral generator (modified)
+				String temp = input.substring(i);
+				double[] floatPointResults = machine.isValid(temp);
+				i += floatPointResults[1] - 1;
+				values.push(floatPointResults[0]);
+				if(floatPointResults[0] == -1) {
+					result[0] = -1;
+					return result;
 				}
 				
-				//Maybe call my floatPointFunctionHere?
-				
-				//Add value into stack
 			}
 			
 			
@@ -51,7 +57,9 @@ public class expressionHandle {
 					values.push(
 							applyOperation( operator.pop(),
 											values.pop(),
-											values.pop()));
+											values.pop()
+											)
+							);
 				}
 				
 				operator.push(curr);
@@ -60,12 +68,62 @@ public class expressionHandle {
 		}
 		
 		while(!operator.empty()) {
-			values.push(applyOperation(operator.pop(),
+			values.push(
+					applyOperation(operator.pop(),
 										values.pop(),
-										values.pop()));
+										values.pop()
+										)
+					);
 		}
 		
+		result[1] = values.pop();
+		return result;
+	}
+	
+	
+	public double applyOperation(char operator, double val1, double val2) {
+		switch(operator) {
+		case '+':
+			return val1 + val2;
+		case '-':
+			return val1 - val2;
+		case '*':
+			return val1 * val2;
+		case '/':
+			return val1/val2;
+		}
 		
-		return values.pop();
+		return 0;
+	}
+	
+	public boolean hasPrecedence( char operator1, char operator2) {
+		if( operator2 == '(' || operator2 == ')') {
+			return false;
+		}
+		if((operator1 == '*' || operator1 == '/')&&
+				(operator2 == '+' || operator2 == '-')) {
+			return false;
+		}
+		else
+			return true;
+	}
+	
+	public static void main(String[] args) {
+		
+		expressionHandle x = new expressionHandle();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Brandon Chao Expression Handler");
+		System.out.println("Type 'exit' to terminate");
+		System.out.print(">>>");
+		String input = scan.next();
+		while(!input.equals("exit")) {
+			double[] values = x.evalEx(input);
+			if(values[0] != -1)
+				System.out.println(values[1]);
+			else
+				System.out.println(input + " is not a valid expression");
+			System.out.print(">>>");
+			input = scan.next();
+		}
 	}
 }
---
+++ Project2/src/floatPointValue.java
@@ -0,0 +1,314 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class floatPointValue {
+	
+	
+	public double[] isValid(String x)
+	{
+		double[] resultArray = new double[2];
+		resultArray[0] = -1;
+		resultArray[1] = 0;
+		
+		int size = x.length();
+		int state = 1;
+		
+		char letter;
+		int asciiValue;
+		
+		double value = 0.0;
+		int decimal = 0;
+		
+		int exponent = 0;
+		boolean exponentSign = false;
+		
+		for(int i = 0; i < size; i++) {
+			letter = x.charAt(i);
+			asciiValue = (int) letter;
+			resultArray[1] = resultArray[1] + 1;
+			
+			switch(state) {
+				case 1:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 2;
+						
+						value = (value*10) + (asciiValue-48);
+					}
+					else if( asciiValue == 46 )
+					{
+						state = 3;
+					}
+					else
+						return resultArray;
+					break;
+				case 2:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 2;
+						
+						value = (value*10) + (asciiValue-48);
+					}
+					else if(asciiValue == 68 || asciiValue == 70 ||
+							asciiValue == 100 || asciiValue == 102) {
+						state = 5;
+					}
+					else if( asciiValue == 46 )
+					{
+						state = 7;
+					}
+					else if(asciiValue == 95) {
+						state = 4;
+					}
+					else if(asciiValue == 69 || asciiValue == 101) {
+						state = 9;
+					}
+					else
+						return resultArray;
+					break;
+				case 3:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 6;
+						
+						value = (value*10) + (asciiValue-48);
+						decimal += 1;
+					}
+					else
+						return resultArray;
+					break;
+				case 4:
+					if(asciiValue == 95) {
+						state = 4;
+					}
+					else if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 2;
+						
+						value = (value*10) + (asciiValue-48);
+					}
+					else
+						return resultArray;
+					break;
+				//Modified to handle expressions
+					
+				//Previously was just "FfDd" must be last character in string
+				//Now is, only accepts "+-*/"
+				case 5:
+					//Modified for expressions
+					if(asciiValue == 41 || asciiValue == 42 || asciiValue == 43 ||
+							asciiValue == 45 || asciiValue == 47) {
+						i = size;
+						if(asciiValue == 41)
+							resultArray[1] = resultArray[1] - 1;
+					}
+					else{
+						return resultArray;
+					}
+					break;
+				//Modified to handle expressions
+				case 6:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 6;
+						
+						value = (value*10) + (asciiValue-48);
+						decimal += 1;
+					}
+					//Modified for expressions
+					else if(asciiValue == 41 || asciiValue == 42 || asciiValue == 43 ||
+							asciiValue == 45 || asciiValue == 47) {
+						i = size;
+					}
+					else if(asciiValue == 95) {
+						state = 8;
+					}
+					else if(asciiValue == 68 || asciiValue == 70 ||
+							asciiValue == 100 || asciiValue == 102) {
+						state = 5;
+					}
+					else if(asciiValue == 69 || asciiValue == 101) {
+						state = 13;
+					}
+					else
+						return resultArray;
+					break;
+				//Modified to handle expressions
+				case 7:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 6;
+						
+						value = (value*10) + (asciiValue-48);
+						decimal += 1;
+					}
+					else if(asciiValue == 69 || asciiValue == 101) {
+						state = 13;
+					}
+					else if(asciiValue == 68 || asciiValue == 70 ||
+							asciiValue == 100 || asciiValue == 102) {
+						state = 5;
+					}
+					//Modified for expressions
+					else if(asciiValue == 41 || asciiValue == 42 || asciiValue == 43 ||
+							asciiValue == 45 || asciiValue == 47) {
+						i = size;
+					}
+					else
+						return resultArray;
+					break;
+				case 8:
+					if(asciiValue == 95) {
+						state = 8;
+					}
+					else if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 6;
+						
+						value = (value*10) + (asciiValue-48);
+						decimal += 1;
+					}
+					else
+						return resultArray;
+					break;
+				case 9:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 11;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					else if(asciiValue == 43 || asciiValue == 45) {
+						state = 10;
+						
+						if(asciiValue == 45) {
+							exponentSign = true;
+						}
+					}
+					else
+						return resultArray;
+					break;
+				case 10:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 11;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					else
+						return resultArray;
+					break;
+				//Modified to handle expressions
+				case 11:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 11;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					//Modified for expressions
+					else if(asciiValue == 41 || asciiValue == 42 || asciiValue == 43 ||
+							asciiValue == 45 || asciiValue == 47) {
+						i = size;
+					}
+					else if(asciiValue == 95) {
+						state = 12;
+					}
+					else if(asciiValue == 68 || asciiValue == 70 ||
+							asciiValue == 100 || asciiValue == 102) {
+						state = 5;
+					}
+					else
+						return resultArray;
+					break;
+				case 12:
+					if(asciiValue == 95) {
+						state = 12;
+					}
+					else if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 11;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					else
+						return resultArray;
+					break;
+				case 13:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 15;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					else if(asciiValue == 43 || asciiValue == 45) {
+						state = 14;
+						
+						if(asciiValue == 45) {
+							exponentSign = true;
+						}
+					}
+					else
+						return resultArray;
+					break;
+				case 14:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 15;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					else
+						return resultArray;
+					break;
+				//Modified to handle expressions
+				case 15:
+					if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 15;
+						
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					//Modified for expressions
+					else if(asciiValue == 41 || asciiValue == 42 || asciiValue == 43 ||
+							asciiValue == 45 || asciiValue == 47) {
+						i = size;
+					}
+					else if(asciiValue == 68 || asciiValue == 70 ||
+							asciiValue == 100 || asciiValue == 102) {
+						state = 5;
+					}
+					else if(asciiValue == 95) {
+						state = 16;
+					}
+					else
+						return resultArray;
+					break;
+				case 16:
+					if(asciiValue == 95) {
+						state = 16;
+					}
+					else if(asciiValue >= 48 && asciiValue <= 57) {
+						state = 15;
+							
+						exponent = (exponent*10) + (asciiValue-48);
+					}
+					else
+						return resultArray;
+					break;
+			}
+			if(i == size-1)
+				resultArray[1] += 1;
+		}
+		if(		state == 1 ||
+				state == 2 || 
+				state == 3 || state == 4 ||
+				state == 8 || state == 9 ||
+				state == 10 || state == 12 ||
+				state == 13 || state == 14 ||
+				state == 16
+		) {
+			return resultArray;
+		}
+		if(state == 5)
+		{
+			resultArray[1] += 1;
+		}
+		value = value * Math.pow(10, -1* decimal);
+		if(exponentSign)
+			value = value * Math.pow(10,-1* exponent);
+		else
+			value = value * Math.pow(10, exponent);
+		
+		resultArray[0] = value;
+		resultArray[1] = resultArray[1] - 1;
+		return resultArray;
+	}
+	
+
+}
